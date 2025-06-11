c589027e3d6998d1a72039f2efeda2040af7ef03
Subhanshu2406 <178010453+Subhanshu2406@users.noreply.github.com>
2025-06-12 03:16:57 +0530

    Added book addition divs, other styling changes.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
+console.log("Script loaded");
+console.log(document.querySelector('.addBook'));
+console.log("Container:", document.querySelector(".mainContainer"));
+
 const addBook = document.querySelector('.addBook');
 const dialogBox = document.querySelector('.dialogBox');
 const form = document.querySelector('form');
+
+const bookIds = [];
 
 
 addBook.addEventListener("click", (e) => {
@@ -14,6 +20,7 @@ console.log("Form submitted");
     const authorname = document.querySelector('.authorinput').value;
     const read = document.querySelector('.checkboxinput').checked;
 
+    console.log("Title:", titlename, "Author:", authorname, "Read:", read);
     createBook(titlename,authorname,read);
 
     form.reset();
@@ -35,9 +42,11 @@ function createBook(titlename,authorname,isRead){
 
     readBox.type = "checkbox";
     readBox.checked = isRead;
-    readBox.disabled = true;
+    readBox.classList.add("readCheckbox");
+
 
     title.textContent = `Title : ${titlename}`;
     author.textContent = `Author : ${authorname}`;
 
 }
+
